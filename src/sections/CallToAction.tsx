0b802cea6223ded4da6172a922e4ec0c2cfe44a2
Angelica Toloza <atolozafranco13@gmail.com>
2024-09-12 16:27:56 -0500

    add global css

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,39 @@
+import ArrowRight from "@/assets/arrow-right.svg";
+import StarImage from "@/assets/star.png";
+import SpringImage from "@/assets/spring.png";
+import Image from "next/image";
 export const CallToAction = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="section-heading relative">
+          <h2 className="section-title mt-5">Sign up for free today</h2>
+          <p className="section-description mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts.
+          </p>
+          <Image
+          src={StarImage}
+          alt="Star Image"
+          height={360}
+          className="hidden md:block absolute -top-[137px] -left-[350px]" 
+        />
+        <Image
+          src={SpringImage}
+          alt="Spring Image"
+          height={360}
+          className="hidden md:block absolute -right-[331px] top-[19px]"
+        />
+        </div>
+        <div className="flex gap-2 mt-10 justify-center ">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text gap-1">
+            <span>Learn more</span>
+            <ArrowRight className="h-5 w-5" />
+          </button>
+        </div>
+        
+      </div>
+    </section>
+  );
 };
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,42 @@
+import ProductImage from "@/assets/product-image.png";
+import PyramidImage from "@/assets/pyramid.png";
+import TubeImage from "@/assets/tube.png";
+import Image from "next/image";
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="section-heading">
+          <div className="flex justify-center">
+            <div className="tag">Boost your productivity</div>
+          </div>
+          <h2 className="section-title mt-5">
+            A more effective way to track progress
+          </h2>
+          <p className="section-description mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts.
+          </p>
+        </div>
+        <div className="relative">
+          <Image src={ProductImage} alt="Product Image" className="mt-10" />
+          <Image
+            src={PyramidImage}
+            alt="Pymarid Image"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+          />
+          /
+          <Image
+            src={TubeImage}
+            alt="Tube Image"
+            height={248}
+            className="hidden md:block absolute bottom-24 -left-36"
+          />
+          /
+        </div>
+      </div>
+    </section>
+  );
 };
