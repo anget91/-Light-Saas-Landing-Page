2568b7d4337cbeb9b9894c6138fa50366ef1cd3a
Angelica Toloza <atolozafranco13@gmail.com>
2024-09-13 11:59:12 -0500

    add CallToAction animation

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,10 +1,21 @@
+"use client";
 import ArrowRight from "@/assets/arrow-right.svg";
 import StarImage from "@/assets/star.png";
 import SpringImage from "@/assets/spring.png";
-import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 export const CallToAction = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip"
+    >
       <div className="container">
         <div className="section-heading relative">
           <h2 className="section-title mt-5">Sign up for free today</h2>
@@ -12,18 +23,26 @@ export const CallToAction = () => {
             Celebrate the joy of accomplishment with an app designed to track
             your progress and motivate your efforts.
           </p>
-          <Image
-          src={StarImage}
-          alt="Star Image"
-          height={360}
-          className="hidden md:block absolute -top-[137px] -left-[350px]" 
-        />
-        <Image
-          src={SpringImage}
-          alt="Spring Image"
-          height={360}
-          className="hidden md:block absolute -right-[331px] top-[19px]"
-        />
+          <motion.img
+            src={StarImage.src}
+            alt="Star Image"
+            height={360}
+            width={360}
+            className="hidden md:block absolute -top-[137px] -left-[350px]"
+            style={{
+              translateY: translateY,
+            }}
+          />
+          <motion.img
+            src={SpringImage.src}
+            alt="Spring Image"
+            height={360}
+            width={360}
+            className="hidden md:block absolute -right-[331px] top-[19px]"
+            style={{
+              translateY: translateY,
+            }}
+          />
         </div>
         <div className="flex gap-2 mt-10 justify-center ">
           <button className="btn btn-primary">Get for free</button>
@@ -32,7 +51,6 @@ export const CallToAction = () => {
             <ArrowRight className="h-5 w-5" />
           </button>
         </div>
-        
       </div>
     </section>
   );
